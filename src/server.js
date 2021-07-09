a03e26506b1f865b0164f29bcc280d5ed13001c7
Harsh Patel <harsh.coderc@gmail.com>
2021-07-09 21:44:05 +0530

    Added custom logger

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const logger = require("./utils/logger");
 
 const app = express();
 
@@ -7,5 +8,5 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(4000, () => {
-    console.log("Server is running on port 4000...");
+    logger.info("Server is running on port 4000...");
 });
---
+++ src/utils/logger.js
@@ -0,0 +1,64 @@
+/**
+ * @author @harsh2124
+ * @description Logger configuration.
+ */
+
+const { createLogger, format, transports } = require("winston");
+
+const options = {
+    /**
+     * @description Options for writing logs to file.
+     * @file logs/server.log
+     */
+    file: {
+        filename: "logs/server.log",
+        maxsize: 5242880, // 5MB
+        maxFiles: 5,
+        handleExceptions: true,
+        format: format.combine(
+            format.timestamp({
+                format: "DD-MMM-YYYY HH:mm:ss",
+            }),
+            format.align(),
+            format.printf(
+                info => `${info.level}: ${[info.timestamp]}: ${info.message}`
+            )
+        ),
+    },
+
+    /**
+     * @description Options for logs to console.
+     */
+    console: {
+        handleExceptions: true,
+        format: format.combine(
+            format.colorize(),
+            format.timestamp({
+                format: "DD-MMM-YYYY HH:mm:ss",
+            }),
+            format.align(),
+            format.printf(
+                info => `${info.level}: ${[info.timestamp]}: ${info.message}`
+            )
+        ),
+    },
+};
+
+const fileTransport = new transports.File(options.file);
+const consoleTransport = new transports.Console(options.console);
+
+const logger = createLogger({
+    transports:
+        process.env.ENV === "DEV"
+            ? [consoleTransport]
+            : [fileTransport, consoleTransport],
+    exitOnError: false,
+});
+
+logger.stream = {
+    write(message) {
+        logger.info(message);
+    },
+};
+
+module.exports = logger;
