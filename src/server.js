b4886eed3630612432c4893570090a1a49501f6c
Harsh Patel <harsh.coderc@gmail.com>
2021-07-11 15:50:21 +0530

    Tweak

--- src/server.js
+++ src/server.js
@@ -13,13 +13,9 @@ const routes = require("./routes/routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const corsOptions = {
-    origin: "*",
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(
     morgan(process.env.ENV === "DEV" ? "dev" : "combined", {
@@ -36,6 +32,30 @@ connectDB().then(() => {
      */
     app.listen(PORT, () => {
         logger.info(`Server is running on port ${PORT}...`);
+    });
+
+    app.use(function (req, res, next) {
+        // Website you wish to allow to connect
+        res.setHeader("Access-Control-Allow-Origin", "*");
+
+        // Request methods you wish to allow
+        res.setHeader(
+            "Access-Control-Allow-Methods",
+            "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+        );
+
+        // Request headers you wish to allow
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "X-Requested-With,content-type"
+        );
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader("Access-Control-Allow-Credentials", true);
+
+        // Pass to next layer of middleware
+        next();
     });
 
     app.use("/api", routes);
