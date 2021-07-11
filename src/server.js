dd479b490d082776ca6a1fdb9a80d2d292bfe22b
Harsh Patel <harsh.coderc@gmail.com>
2021-07-11 15:56:19 +0530

    Tweak

--- src/server.js
+++ src/server.js
@@ -15,6 +15,31 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(cors());
 
 app.use(
@@ -32,30 +57,6 @@ connectDB().then(() => {
      */
     app.listen(PORT, () => {
         logger.info(`Server is running on port ${PORT}...`);
-    });
-
-    app.use(function (req, res, next) {
-        // Website you wish to allow to connect
-        res.setHeader("Access-Control-Allow-Origin", "*");
-
-        // Request methods you wish to allow
-        res.setHeader(
-            "Access-Control-Allow-Methods",
-            "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-        );
-
-        // Request headers you wish to allow
-        res.setHeader(
-            "Access-Control-Allow-Headers",
-            "X-Requested-With,content-type"
-        );
-
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader("Access-Control-Allow-Credentials", true);
-
-        // Pass to next layer of middleware
-        next();
     });
 
     app.use("/api", routes);
