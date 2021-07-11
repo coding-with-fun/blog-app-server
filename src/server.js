fc250efbf02af3f400741f35576f80e2681eafa7
Harsh Patel <harsh.coderc@gmail.com>
2021-07-11 15:43:28 +0530

    Tweak

--- src/server.js
+++ src/server.js
@@ -55,5 +55,14 @@ connectDB().then(() => {
         logger.info(`Server is running on port ${PORT}...`);
     });
 
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+    });
+
     app.use("/api", routes);
 });
